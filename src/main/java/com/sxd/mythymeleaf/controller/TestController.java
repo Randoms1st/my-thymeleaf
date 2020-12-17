db584a32fe1290a458b04d579f26587190b9253d
offcn-java <1833930673@qq.com>
2020-12-17 17:37:25 +0800

    我造了一个Controller

---
+++ src/main/java/com/sxd/mythymeleaf/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sxd.mythymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/getgit")
+    public String getgit(){
+        return "添加的一个Controller！！！";
+    }
+}
